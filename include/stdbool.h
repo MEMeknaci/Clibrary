b40fdee7ffe22181e04e2d6b246e1d41da47eed9
Mehdi Meknaci <mehdi.meknaci@epitech.eu>
2020-02-14 16:34:07 -0500

    Added header

---
+++ include/stdbool.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** stdbool.h
+** File description:
+** stdbool lib
+*/
+
+#ifndef __bool_true_false_are_defined
+#define stdbool
+
+#define true        1
+#define false       !(true)
+
+#endif /* stdbool */
