93a719b98f6a7269c90309df9adbd8d3f6160748
Mehdi Meknaci <mehdi.meknaci@epitech.eu>
2020-02-14 06:20:56 -0500

    Added errno lib

---
+++ errno/my_errno.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2020
+** errno
+** File description:
+** errno lib
+*/
+
+#include "errno.h"
+
+void putstring(char *str)
+{
+    int i = 0;
+
+    for (; str[i]; i++);
+    write(1, str, i);
+}
+
+void my_errno(void)
+{
+    if (EDOM == 33)
+        putstring("EDOM 33\n");
+    if (ERANGE == 34)
+        putstring("ERANGE 33\n");
+    if (EIO == 5)
+        putstring("EIO 5\n");
+    if (ENAMETOOLONG == 63)
+        putstring("ENAMETOOLONG 63\n");
+    if (EPERM == 1)
+        putstring("EPERM 1\n");
+    if (ENETDOWN == 50)
+        putstring("ENETDOWN 50\n");
+}
