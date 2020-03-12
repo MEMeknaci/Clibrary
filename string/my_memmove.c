2a5d441cb4ca63841432a078c9abd84533609f02
Mehdi <mehdi.meknaci@epitech.eu>
2020-03-12 11:10:23 +0100

    Fix memmove

--- string/my_memmove.c
+++ string/my_memmove.c
@@ -10,17 +10,17 @@
 
 void my_memmove(void const *dest, void const *src, size_t size)
 {
-    char *string_src = (char *) src;
-    char *string_dest = (char *) dest;
-    char *temp = malloc(sizeof(char) * (size + 1));
-    size_t j = 0;
+    char *dest_s = (char *) dest;
+    char *src_s = (char *) src;
+    char *buffer = malloc(sizeof(char) * (size + 1));
+    size_t i = 0;
 
-    if (temp == NULL)
+    if (buffer == NULL)
         return;
-    for (j = 0; j < size; j++)
-        temp[j] = string_src[j];
-    temp[j] = 0;
-    for (size_t i = 0; i < size; i++)
-        string_dest[i] = temp[i];
-    free(temp);
+    for (i = 0; i < size; i++)
+        buffer[i] = src_s[i];
+    buffer[i] = 0;
+    for (i = 0; i < size; i++)
+        dest_s[i] = buffer[i];
+    free(buffer);
 }
